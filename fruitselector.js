d68a36b8c8f80e8e62edaecde69d12e117b3d965
divinitywings <sonalovesbubbles@gmail.com>
2024-07-02 17:47:00 -0400

    Add files via upload

---
+++ fruitselector.js
@@ -0,0 +1,35 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+	for(let i = 0; i<fruit.length; i++){
+		if(fruit[i].search(new RegExp(str, 'i')) != -1) {
+			results.push(fruit[i]);
+		}
+	}
+	return results;
+}
+
+function searchHandler(e) {
+	showSuggestions(search(input.value), input.value);
+}
+
+function showSuggestions(results, inputVal) {
+	suggestions.innerHTML = "";
+	for(let i=0; i<results.length; i++){
+		var li = document.createElement('li');
+		li.appendChild(document.createTextNode(results[i]));
+		suggestions.appendChild(li);
+	}
+}
+
+function useSuggestion(e) {
+	input.value = e.target.textContent;
+	suggestions.innerHTML = "";
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
